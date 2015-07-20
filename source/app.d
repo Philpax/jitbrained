ab66f8403dbbd375ae74bf0a2a582f86616c13e1
Philpax <me@philpax.me>
2015-07-20 17:42:28 +1000

    Wrote a basic optimizing compiler that folds sequential instructions

--- source/app.d
+++ source/app.d
@@ -2,6 +2,7 @@ import std.stdio;
 import std.array;
 import std.conv;
 import std.file;
+import std.algorithm;
 
 import idjit;
 
@@ -84,6 +85,151 @@ BasicBlock compileNaiveBF(string input)
     return block;
 }
 
+BasicBlock compileOptimizedBF(string input)
+{
+    BasicBlock block;
+
+    enum Opcode
+    {
+        Add,
+        Subtract,
+        Forward,
+        Backward,
+        Output,
+        Input,
+        LeftBracket,
+        RightBracket
+    }
+
+    struct Instruction
+    {
+        Opcode opcode;
+        int value;
+    }
+
+    uint labelIndex = 0;
+    uint[] labelStack;
+    Instruction[] ir;
+
+    void addFoldableInstruction(Opcode opcode)
+    {
+        if (ir.length && ir[$-1].opcode == opcode)
+            ir[$-1].value++;
+        else
+            ir ~= Instruction(opcode, 1);
+    }
+
+    // Build up IR
+    foreach (c; input)
+    {
+        // *ptr++
+        if (c == '+')
+            addFoldableInstruction(Opcode.Add);
+        // *ptr--
+        else if (c == '-')
+            addFoldableInstruction(Opcode.Subtract);
+        // ptr++
+        else if (c == '>')
+            addFoldableInstruction(Opcode.Forward);
+        // ptr--
+        else if (c == '<')
+            addFoldableInstruction(Opcode.Backward);
+        // putchar(*ptr)
+        else if (c == '.')
+            ir ~= Instruction(Opcode.Output, 0);
+        // *ptr = getchar()
+        else if (c == ',')
+            ir ~= Instruction(Opcode.Input, 0);
+        // while (*ptr) {
+        else if (c == '[')
+        {
+            ir ~= Instruction(Opcode.LeftBracket, labelIndex);
+
+            // Push back the current index to the label stack
+            labelStack ~= labelIndex;
+            ++labelIndex;
+        }
+        // }
+        else if (c == ']')
+        {
+            // Grab the last label off the stack, and use it
+            ir ~= Instruction(Opcode.RightBracket, labelStack[$-1]);
+            labelStack.length--;
+        }
+    }
+
+    // Dump out IR
+    ir.each!writeln();
+
+    // Build machine code
+    with (block) with (Register) with (OperandType)
+    {
+        // Load in array at EBX, as the D ABI guarantees it won't be 
+        // trampled by function calls
+        mov(EBX, _(EBP, 8));
+
+        foreach (instruction; ir)
+        {
+            if (instruction.opcode == Opcode.Add)
+            {
+                add(_(Byte, EBX), cast(byte)instruction.value);
+            }
+            else if (instruction.opcode == Opcode.Subtract)
+            {
+                sub(_(Byte, EBX), cast(byte)instruction.value);
+            }
+            else if (instruction.opcode == Opcode.Forward)
+            {
+                // Avoid emitting a large immediate where possible
+                if (instruction.value.fitsIn!byte)
+                    add(EBX, cast(byte)instruction.value);
+                else
+                    add(EBX, instruction.value);
+            }
+            else if (instruction.opcode == Opcode.Backward)
+            {
+                // Avoid emitting a large immediate where possible
+                if (instruction.value.fitsIn!byte)
+                    sub(EBX, cast(byte)instruction.value);
+                else
+                    sub(EBX, instruction.value);
+            }
+            else if (instruction.opcode == Opcode.Output)
+            {
+                mov(EAX, _(Byte, EBX));
+                call(&putchar);
+            }
+            else if (instruction.opcode == Opcode.Input)
+            {
+                call(&getchar);
+                mov(_(Byte, EBX), EAX);
+            }
+            else if (instruction.opcode == Opcode.LeftBracket)
+            {
+                // Generate label
+                auto labelString = instruction.value.to!string();
+
+                mov(EAX, _(Byte, EBX));
+                cmp(EAX, 0);
+                je("r" ~ labelString);
+                label("l" ~ labelString);
+            }
+            else if (instruction.opcode == Opcode.RightBracket)
+            {
+                // Grab the last label off the stack, and use it
+                auto labelString = instruction.value.to!string();
+
+                mov(EAX, _(Byte, EBX));
+                cmp(EAX, 0);
+                jne("l" ~ labelString);
+                label("r" ~ labelString);
+            }
+        }
+    }
+
+    return block;
+}
+
 void main(string[] args)
 {
     if (args.length < 2)
@@ -108,7 +254,7 @@ void main(string[] args)
         ret;
     }
 
-    auto assembly = Assembly(preludeBlock, testString.compileNaiveBF(), endBlock);
+    auto assembly = Assembly(preludeBlock, testString.compileOptimizedBF(), endBlock);
     assembly.finalize();
     writeln("Byte count: ", assembly.buffer.length);
     writeln("-------");
