6c54a2ad198a1027a0d621018a8652ba31933fc4
Philpax <me@philpax.me>
2015-07-21 17:36:31 +1000

    Removed naive compilation method; added --no-optimize to act as the same as naive compilation

--- source/app.d
+++ source/app.d
@@ -6,86 +6,7 @@ import std.algorithm;
 
 import idjit;
 
-BasicBlock compileNaiveBF(string input)
-{
-    BasicBlock block;
-    uint labelIndex = 0;
-    uint[] labelStack;
-
-    with (block) with (Register) with (OperandType)
-    {
-        // Load in array at EBX, as the D ABI guarantees it won't be 
-        // trampled by function calls
-        mov(EBX, _(EBP, 8));
-
-        foreach (c; input)
-        {
-            // *ptr++
-            if (c == '+')
-            {
-                add(_(Byte, EBX), 1);
-            }
-            // *ptr--
-            else if (c == '-')
-            {
-                sub(_(Byte, EBX), 1);
-            }
-            // ptr++
-            else if (c == '>')
-            {
-                inc(EBX);
-            }
-            // ptr--
-            else if (c == '<')
-            {
-                dec(EBX);
-            }
-            // putchar(*ptr)
-            else if (c == '.')
-            {
-                mov(EAX, _(Byte, EBX));
-                call(&putchar);
-            }
-            // *ptr = getchar()
-            else if (c == ',')
-            {
-                call(&getchar);
-                mov(_(Byte, EBX), EAX);
-            }
-            // while (*ptr) {
-            else if (c == '[')
-            {
-                // Generate label
-                auto labelString = labelIndex.to!string();
-
-                mov(EAX, _(Byte, EBX));
-                cmp(EAX, 0);
-                je("r" ~ labelString);
-                label("l" ~ labelString);
-
-                // Push back the current index to the label stack
-                labelStack ~= labelIndex;
-                ++labelIndex;
-            }
-            // }
-            else if (c == ']')
-            {
-                // Grab the last label off the stack, and use it
-                auto labelString = labelStack[$-1].to!string();
-                labelStack.length--;
-
-                mov(EAX, _(Byte, EBX));
-                cmp(EAX, 0);
-                jne("l" ~ labelString);
-                label("r" ~ labelString);
-            }
-        }
-    }
-
-    return block;
-}
-
-BasicBlock compileOptimizedBF(string input)
+BasicBlock compileBF(string input, bool optimize)
 {
     BasicBlock block;
 
@@ -113,7 +34,7 @@ BasicBlock compileOptimizedBF(string input)
 
     void addFoldableInstruction(Opcode opcode)
     {
-        if (ir.length && ir[$-1].opcode == opcode)
+        if (ir.length && ir[$-1].opcode == opcode && optimize)
             ir[$-1].value++;
         else
             ir ~= Instruction(opcode, 1);
@@ -258,7 +179,9 @@ void main(string[] args)
         ret;
     }
 
-    auto assembly = Assembly(preludeBlock, testString.compileOptimizedBF(), endBlock);
+    bool optimize = !args.canFind("--no-optimize");
+
+    auto assembly = Assembly(preludeBlock, testString.compileBF(optimize), endBlock);
     assembly.finalize();
     writeln("Byte count: ", assembly.buffer.length);
     writeln("-------");
